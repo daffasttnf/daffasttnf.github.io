5c62f3245e467f3214adc22fb39d16820a71adf5
webprogrammingdaffa <daffaariftamareal@gmail.com>
2025-10-10 17:27:24 +0700

    Fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
   server: {
     host: true,
     port: 3000,
   },
+  build: {
+    outDir: 'dist'
+  },
+  // Tambahkan ini untuk SPA routing di production
+  base: './'
 })
